93e59088890e335454c17dfcc59cffc230befb28
ziye_huang <ziye_huang@163.com>
2018-08-29 19:35:28 +0800

    修改底部导航栏增加忽略点击事件的操作。

--- views/src/main/java/com/hzy/layout/TabBarLayout.java
+++ views/src/main/java/com/hzy/layout/TabBarLayout.java
@@ -119,8 +119,8 @@ public class TabBarLayout extends LinearLayout implements ViewPager.OnPageChange
 
         @Override
         public void onClick(View v) {
-            if (-1 != ignoreIndex && ignoreIndex == mCurrentItem) {
-                mItemViews.get(mCurrentItem).setOnClickListener(ignoreClickListener);
+            if (-1 != ignoreIndex && ignoreIndex == currentIndex) {
+                mItemViews.get(currentIndex).setOnClickListener(ignoreClickListener);
                 return;
             }
 
